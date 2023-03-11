1617f4e08c6b1bbf0beecfb5309b17308d480e48
rajany39 <rajany90@yahoo.com>
2023-03-11 11:15:55 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [hori,sethori]=useState(10)
+  const [veri,setveri]=useState(10)
+  const [blur,setblur]=useState(10)
+  const [color,setcolor]=useState("black")
+  const [checkon,setcheckon]=useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="controls">
+        <label htmlFor="hori">Horizontal</label>
+        <input type="range"  min="-200" max="200" value={hori}  onChange={(e)=>sethori(e.target.value)} />
+        
+        <label htmlFor="veri">Vertical</label>
+        <input type="range"  min="-200" max="200" value={veri}  onChange={(e)=>setveri(e.target.value)} />
+        
+        <label htmlFor="blur">Blur</label>
+        <input type="range"  min="-200" max="200" value={blur}  onChange={(e)=>setblur(e.target.value)} />
+
+        <label htmlFor="color">Color</label>
+        <input type="color"   value={color}  onChange={(e)=>setcolor(e.target.value)} />
+
+        <div className="switch">
+          <label >
+            Outline
+            <input type="checkbox" value={checkon} onChange={()=>{setcheckon(!checkon)}} />
+            <span className="lever"></span>
+            inset
+          </label>
+        </div>
+        
+      </div>
+
+      <div className="output">
+        <div className="box" style={{boxShadow:` ${checkon?"inset":""} ${hori}px ${veri}px ${blur}px  ${color} `}}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          boxShadow:{hori}px {veri}px {blur}px {color}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </div>
+      </div>
+      
     </div>
   );
 }
